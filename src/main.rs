86fd9e7f2f96e508c4a73359692bcab4beaec8fe
Michael Walczyk <michael.walczyk@obscuradigital.com>
2018-02-05 11:47:13 -0800

    Integrates clap command-line argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate image;
 extern crate pbr;
+extern crate clap;
 
 mod helpers;
 mod fluid_quantity;
@@ -7,45 +8,69 @@ mod fluid_solver;
 mod render;
 
 use std::env;
+use std::str::FromStr;
+
 use fluid_quantity::FluidQuantity;
 use fluid_solver::FluidSolver;
+
 use pbr::ProgressBar;
+use clap::{Arg, App, SubCommand};
 
 // TODO:
 // https://en.wikipedia.org/wiki/Stencil_code
 // http://developer.download.nvidia.com/books/HTML/gpugems/gpugems_ch38.html
 
-static IMAGE_DIR: &'static str = "images";
-const TOTAL_FRAMES: usize = 6000;
 const ADD_FLUID_EVERY: usize = 500;
 
-fn help() {
-    println!("Ponyo - a 2D, semi-Lagrangian fluid solver");
-}
-
 fn main() {
-    help();
-
     let sp = render::shader_program::ShaderProgram { id: 32 };
     sp.test();
 
-    // Parse command-line arguments:
-    //
-    // --output_dir -o
-    // --resolution -r
-    // --frame_count -f
-    // --density -d
-    // ...
-    let args: Vec<String> = env::args().collect();
-    match args.len() {
-        1 => (),
-        2 => (),
-        _ => help()
-    }
+    // Parse command-line arguments.
+    let matches = App::new("Ponyo")
+        .version("1.0")
+        .author("Mike Walczyk <mwalczyk2@gmail.com>")
+        .about("A semi-Lagrangian fluid solver")
+        .arg(Arg::with_name("output_directory")
+            .short("o")
+            .long("output_directory")
+            .value_name("DIRECTORY")
+            .help("Sets the directory where images will be rendered to")
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("frame_count")
+            .short("f")
+            .long("frame_count")
+            .value_name("COUNT")
+            .help("Sets the number of frames that will be rendered")
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("resolution")
+            .short("r")
+            .long("resolution")
+            .value_name("WIDTHxHEIGHT")
+            .help("Sets the resolution of the simulation, in pixels")
+            .takes_value(true)
+            .use_delimiter(true)
+            .value_delimiter("x")
+        ).get_matches();
+
+    let v = Arg::with_name("v");
+
+    // Print the final program arguments.
+    let output_directory = matches.value_of("output_directory").unwrap_or("images");
+    println!("Setting output directory to: {}", output_directory);
+
+    let frame_count = usize::from_str(matches.value_of("frame_count").unwrap_or("6000")).unwrap();
+    println!("Setting frame count to: {}", frame_count);
+
+    let resolution = matches.value_of("resolution").unwrap_or("128x128");
+    let dims: Vec<&str> = resolution.split("x").collect();
+    let w = usize::from_str(dims[0]).unwrap();
+    let h = usize::from_str(dims[1]).unwrap();
+    println!("Setting resolution to: {}x{}", w, h);
 
     // Create and initialize a new solver.
-    let w = 128_usize;
-    let h = 128_usize;
     let mut solver = FluidSolver::new(w, h);
 
     // Parameters for adding new fluid.
@@ -57,15 +82,15 @@ fn main() {
     let v = 0.0;
 
     // Initialize the progress bar.
-    let mut progress = ProgressBar::new(TOTAL_FRAMES as u64);
+    let mut progress = ProgressBar::new(frame_count as u64);
 
     // Start the simulation.
-    for i in 0..TOTAL_FRAMES {
+    for i in 0..frame_count {
         // Increment the progress bar.
         progress.inc();
 
         // Add some fluid, except on the last frame.
-        if i % ADD_FLUID_EVERY == 0 && i != TOTAL_FRAMES {
+        if i % ADD_FLUID_EVERY == 0 && i != frame_count {
             solver.add_source(upper_left_x,
                               upper_left_y,
                               block_w,
@@ -76,7 +101,7 @@ fn main() {
 
         // Update the solver and save out frames to disk.
         solver.update();
-        solver.to_image(&format!("{}/frame_{}.png", IMAGE_DIR, i));
-        //println!("Completed iteration: {}", i);
+        solver.to_image(&format!("{}/frame_{}.png", output_directory, i));
+        println!("Completed iteration: {}", i);
     }
 }
